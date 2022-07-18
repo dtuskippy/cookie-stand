d00bebd41ae94a637215632b41ced89640938b73
Alan Chelko <achelko@gmail.com>
2022-07-19 01:33:40 +0200

    Finalized first object

--- js/app.js
+++ js/app.js
@@ -1,45 +1,119 @@
 'use strict';
 
+let salesSection = document.getElementById('store-sales-data');
+
 let seattle = {
-  'Location': 'Seattle',
-  'Min / Cust': 23,
-  'Max / Cust': 65,
-  'Avg Cookie / Sale': 6.3
-};
+  location: 'Seattle',
+  minCust: 23, 
+  maxCust: 65, 
+  aveCookieSal: 6.3, 
+  openingHour: 6,
+  closingHour: 19,
+  salesData: 0,
+  customerCalc: function(){
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+  },
+  salesCount: function(){
+    let arr = [];  
+    for(let i = this.openingHour; i <= this.closingHour; i++) {
+        let obj = {};
+        let salesCalc = Math.floor(this.customerCalc() * this.aveCookieSal);
+      if(i < 12) {
+        obj[i + "am"] = salesCalc;
+      } else if(i === 12){
+        obj[i + "pm"] = salesCalc;
+      }else {
+        obj[i - 12 + "pm"] = salesCalc;
+      }
+        arr.push(obj);
+      
+        }
+    return arr;
+  },
+  render: function(){
+    let articleElem = document.createElement('article');
+    salesSection.appendChild(articleElem);
+        
+    let h2Elem = document.createElement('h2');
+    h2Elem.textContent = this.location;
+    articleElem.appendChild(h2Elem);
+
+    let ulElem = document.createElement('ul');
+    articleElem.appendChild(ulElem);
+
+    for(let i = 0; i < this.salesData.length; i++){
+      let innerObj = this.salesData[i];
+      for(let key in innerObj) {
+        let liElem = document.createElement('li');
+        liElem.textContent = `${key}: ${innerObj[key]}`;
+        ulElem.appendChild(liElem);
+      }
+      
+    }
+
+    
+  }
+  
+}
+
+seattle.salesData = seattle.salesCount();
+seattle.render();
+console.log(seattle.salesData);
 
 
-let tokyo = {
-  'Location': 'Tokyo',
-  'Min / Cust': 3,
-  'Max / Cust': 24,
-  'Avg Cookie / Sale': 1.2
-};
-
-let dubai = {
-  'Location': 'Dubai',
-  'Min / Cust': 11,
-  'Max / Cust': 38,
-  'Avg Cookie / Sale': 3.7
-};
+// let seattle = {
+//   location: 'Seattle',
+//   minCust: 23,
+//   maxCust: 65,
+//   aveCookieSale: 6.3,
+//   customerCalc: function(){
+//     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+//   },
+    
+// }
 
 
 
-let paris = {
-  'Location': 'Paris',
-  'Min / Cust': 20,
-  'Max / Cust': 38,
-  'Avg Cookie / Sale': 2.3
-};
+// let tokyo = {
+//   location': 'Tokyo',
+//   minCust: 3,
+//   maxCust: 24,
+//   aveCookieSale: 1.2,
+//   method: function(){};,
+//   calcArr: []
+// };
 
-let lima = {
-  'Location': 'Lima',
-  'Min / Cust': 2,
-  'Max / Cust': 16,
-  'Avg Cookie / Sale': 4.6
-};
+// let dubai = {
+//   'Location': 'Dubai',
+//   'Min / Cust': 11,
+//   'Max / Cust': 38,
+//   'Avg Cookie / Sale': 3.7,
+//   method: function(){};,
+//   calcArr: []
+// };
 
 
 
+// let paris = {
+//   'Location': 'Paris',
+//   'Min / Cust': 20,
+//   'Max / Cust': 38,
+//   'Avg Cookie / Sale': 2.3,
+//   method: function(){};,
+//   calcArr: []
+// };
+
+// let lima = {
+//   'Location': 'Lima',
+//   'Min / Cust': 2,
+//   'Max / Cust': 16,
+//   'Avg Cookie / Sale': 4.6,
+//   method: function(){};,
+//   calcArr: []
+// };
+
+
+// section: store-sales-data
 
 
 /*
